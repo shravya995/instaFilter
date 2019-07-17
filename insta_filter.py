88748c470d18c4c391224ce38bf2ef0755404dc8
Shrawya <shravya995@gmail.com>
2019-07-17 20:06:54 +0530

    comment statements

--- insta_filter.py
+++ insta_filter.py
@@ -1,16 +1,16 @@
 from PIL import Image
-
+#open the images
 base_img=Image.open("image2.jpg")
 image_filter=Image.open("filter1.jpg")
 
 size=(760,760)
-
+#resize the images to the same size
 base_img=base_img.resize(size)
 image_filter=image_filter.resize(size)
-
+#split the images in RGB format
 r,g,b=base_img.split()
 R,G,B=image_filter.split()
-
+#Merge the images
 im=Image.merge("RGB",(g,R,b))
 im.show()
 # im.save('1_merged.jpg')
